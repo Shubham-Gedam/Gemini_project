ff9fd3ae6914a74f94fd61bc000562556bd19e7f
Shubham-Gedam <shubhgedam28@gmail.com>
2026-01-24 23:00:11 +0530

    improve socket server middleware and message handling

--- src/socket/socket.server.js
+++ src/socket/socket.server.js
@@ -6,58 +6,68 @@ import * as aiService from "../services/ai.service.js";
 import messageModel from "../models/message.model.js";
 import { text } from "express";
 
-export function initSocketServer(httpserver) {
-  const io = new Server(httpserver, {});
+export function initSocketServer(httpServer) {
+  const io = new Server(httpServer, {});
 
+  /* Socket.io Middleware  */
   io.use(async (socket, next) => {
     const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
-    if (!cookies.token) {
-      next(new Error("authentication error: token not provide"));
+    const token = cookies.token; // yahan se token milega
+    if (!token) {
+      return next(new Error("Authentication error: No token provided"));
     }
+
     try {
-      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); //  token verify
 
       const user = await userModel.findById(decoded.id);
 
-      socket.user = user;
+      socket.user = user; // user info socket me attach
+
       next();
-    } catch (error) {
-      next(new Error("authentication error: token invalid"));
+    } catch (err) {
+      return next(new Error("Authentication error: Invalid token"));
     }
   });
 
   io.on("connection", (socket) => {
+    //Listner
     socket.on("ai-message", async (messagePayload) => {
       console.log(messagePayload);
 
+      // save user message
       await messageModel.create({
         chat: messagePayload.chat,
-        content: messagePayload.message,
         user: socket.user._id,
+        content: messagePayload.content,
         role: "user",
       });
-      
-      const chatHistory = await messageModel.find({chat: messagePayload.chat});
-      
 
-      const response = await aiService.generateResponse(chatHistory.map(item =>{
-        return{
-            role: item.role,
-            parts: [{text: item.content}]
-        }
-      }));
+      // get ordered history
+      const chatHistory = await messageModel
+        .find({ chat: messagePayload.chat })
+        .sort({ createdAt: 1 });
 
+      const response = await aiService.generateResponse(
+        chatHistory.map((item) => ({
+          role: item.role,
+          parts: [{ text: item.content }],
+        })),
+      );
+
+      // save AI message
       await messageModel.create({
         chat: messagePayload.chat,
-        content: response,
         user: socket.user._id,
+        content: response,
         role: "model",
       });
 
+      // send AI reply
       socket.emit("ai-response", {
-        content: response,
         chat: messagePayload.chat,
+        message: response,
       });
     });
   });
